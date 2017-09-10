86ee396e9c78d522dff04112b7491a89839042e1
yuhaiqing <1243305344@qq.com>
2017-09-10 10:03:51 +0800

    asd

--- README.md
+++
@@ -1,7 +0,0 @@
-# self_test
-
-
-## test
-
-
-# 跳转一定要用 react-router 的link 或者 react-router方法里面的pushstate，否则必然会重新刷新页面，重新加载js，不和谐！！！！跟后台么关系
--- components/js/common/lib.js
+++
@@ -1,8 +0,0 @@
-import 'antd/dist/antd.less';
-import './common.less';
-import './iconfont-lark.less';
-import '../styles/docs.less';
-import './slide.less';
-import './emoji.less';
-import './typo.less';
-import './KaTeX/katex.less';
