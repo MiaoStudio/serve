f207086fb099fc9a9f9c3897f237edef5fcc2efe
yuhaiqing <1243305344@qq.com>
2017-09-10 10:06:39 +0800

    asd

--- README.md
+++
@@ -1,4 +0,0 @@
-# self_test
-
-
-## test
--- components/index.js
+++
@@ -1,5 +0,0 @@
-import React from './js/main.jsx';
-// import World from './World/index.jsx';
-
-// import './Hello/index.less';
-// import './World/index.less';
